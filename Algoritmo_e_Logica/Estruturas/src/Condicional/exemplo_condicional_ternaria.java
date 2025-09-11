f084501ed35a8dba88f702ce4482abfe6c1d94fb
Victormoroo <victorhugo0609@outlook.com>
2025-09-11 15:05:04 -0300

    feat: exemplo ternario e switch case

---
+++ Algoritmo_e_Logica/Estruturas/src/Condicional/exemplo_condicional_ternaria.java
@@ -0,0 +1,23 @@
+package Condicional;
+
+public class exemplo_condicional_ternaria {
+  public static void main(String[] args) {
+    
+    double preco = 34.5;
+    double desconto;
+
+    if (preco < 20.0) {
+      desconto = preco * 0.1;
+    }
+    else {
+      desconto = preco * 0.05;
+    }
+
+    double preco2 = 34.5;
+    double desconto2 = (preco2 < 20.0) ? preco2 * 0.1 : preco2 * 0.05;
+
+    System.out.println(desconto);
+
+    // (condicao) ? valor se verdadeiro : valor se falso
+  }
+}
