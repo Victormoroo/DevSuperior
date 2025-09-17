279d451dd319890f43340a85d0e6062cb018c185
Victormoroo <victorhugo0609@outlook.com>
2025-09-17 15:11:18 -0300

    feat: feito mais exercicios envolvendo classes e pastas renomeadas para facil entendimento

---
+++ Programacao_Moderna/Estoque_pt1/src/application/App.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class App {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Product product = new Product();
+
+    System.out.println("Enter Product data:");
+    System.out.print("Name: ");
+    product.name = sc.nextLine();
+    System.out.print("Price: ");
+    product.price = sc.nextDouble();
+    System.out.print("Quantity in stock: ");
+    product.quantity = sc.nextInt();
+
+    System.out.println("\nProduct data: " + product);
+    
+    System.out.print("\nEnter the number of produts to be added in stock: ");
+    int quantity = sc.nextInt();
+    product.addProducts(quantity);
+
+    System.out.println("\nProduct data: " + product);
+
+    System.out.print("\nEnter the number of produts to be removed in stock: ");
+    quantity = sc.nextInt();
+    product.removeProducts(quantity);
+
+    System.out.println("\nProduct data: " + product);
+
+    sc.close();
+  }
+}
