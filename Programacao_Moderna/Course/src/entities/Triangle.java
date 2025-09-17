0b4a070fea250d880ad32930be47d8305387d925
Victormoroo <victorhugo0609@outlook.com>
2025-09-17 14:24:14 -0300

    feat: finalizado programa para calculo de area de triangulo utilizando POO

--- Programacao_Moderna/Course/src/entities/Triangle.java
+++ Programacao_Moderna/Course/src/entities/Triangle.java
@@ -5,4 +5,13 @@ public class Triangle {
   public double a;
   public double b;
   public double c;
+
+  public double area() {
+    double p = (a + a + c) / 2.0;
+    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    // Tambem poderia ser assim:
+    // double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    // return result;
+  }
 }
