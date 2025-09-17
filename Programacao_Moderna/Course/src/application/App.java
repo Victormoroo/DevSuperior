145af5b9f091bd547b194779752feeea807c6dc9
Victormoroo <victorhugo0609@outlook.com>
2025-09-17 14:15:28 -0300

    feat: programa em processo de refatoramento, trabalhando com POO

--- Programacao_Moderna/Course/src/application/App.java
+++ Programacao_Moderna/Course/src/application/App.java
@@ -1,28 +1,32 @@
-// RESOLUÇÃO SEM UTILIZAR ORIENTAÇÃO A OBJETOS!
-
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+import entities.Triangle;
+
+public class App {
   public static void main(String[] args) {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
+    Triangle x, y;
+    x = new Triangle();
+    y = new Triangle();
+
     System.out.println("Enter the measures of triangle X: ");
-    double xA = sc.nextDouble();
-    double xB = sc.nextDouble();
-    double xC = sc.nextDouble();
+    x.a = sc.nextDouble();
+    x.b = sc.nextDouble();
+    x.c = sc.nextDouble();
 
     System.out.println("Enter the measures of triangle Y: ");
-    double yA = sc.nextDouble();
-    double yB = sc.nextDouble();
-    double yC = sc.nextDouble();
+    y.a = sc.nextDouble();
+    y.b = sc.nextDouble();
+    y.c = sc.nextDouble();
 
-    double p = (xA + xB + xC) / 2.0;
-    double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+    double p = (x.a + x.a + x.c) / 2.0;
+    double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-    p = (yA + yB + yC) / 2.0;
-    double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+    p = (y.a + y.b + y.c) / 2.0;
+    double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
     System.out.printf("Triangle X area: %.4f%n", areaX);
     System.out.printf("Triangle Y area : %.4f%n", areaY);
